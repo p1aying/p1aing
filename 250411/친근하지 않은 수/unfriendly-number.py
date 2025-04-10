83aa2561478c6c7442205cc31a3753c39d5492fc
p1aying <auddn0585@naver.com>
2025-04-11 03:00:14 +0900

    Title: 친근하지 않은 수, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250411/친근하지 않은 수/unfriendly-number.py
+++ 250411/친근하지 않은 수/unfriendly-number.py
@@ -1,9 +1,10 @@
-a=[x for x in range(1,int(input()))]
+a=[x for x in range(int(input()))]
+#print(a)
 b=[]
 
-for i in range(len(a)):
+for i in range(1,len(a)):
     if (a[i]%2==0) or (a[i]%5==0) or (a[i]%3==0):
        b.append(a[i]) 
     else:
         continue
-print(len(a)-len(b))
+print(len(a)-len(b)-1)
