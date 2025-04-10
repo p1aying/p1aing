b600eb12738cae39b705408dc28bd41b183a2caf
p1aying <auddn0585@naver.com>
2025-04-11 01:42:31 +0900

    Title: 중앙값 구하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250411/중앙값 구하기/find-the-median.py
@@ -0,0 +1,19 @@
+a,b,c=map(int,input().split())
+
+if a>b and a>c:
+    if b>c:
+        print(b)
+    else:
+        print(c)
+
+if b>a and b>c:
+    if a>c:
+        print(a)
+    else:
+        print(c)
+
+if c>b and c>a:
+    if b>a:
+        print(b)
+    else:
+        print(a)
