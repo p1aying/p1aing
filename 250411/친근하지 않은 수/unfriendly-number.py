9794229e51ac084b48ddd6f80df448a8def0bc6d
p1aying <auddn0585@naver.com>
2025-04-11 03:00:58 +0900

    Title: 친근하지 않은 수, Time: 40ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250411/친근하지 않은 수/unfriendly-number.py
+++ 250411/친근하지 않은 수/unfriendly-number.py
@@ -1,10 +1,9 @@
-n = int(input())
+a = [x for x in range(int(input()))]
 b = []
 
-for i in range(1, n):  # 1부터 n-1까지
-    if (i % 2 == 0) or (i % 5 == 0) or (i % 3 == 0):
-        b.append(i)
+for i in range(1, len(a)):
+    if (a[i] % 2 == 0) or (a[i] % 5 == 0) or (a[i] % 3 == 0):
+        b.append(a[i])
     else:
         continue
-
-print((n - 1) - len(b))  # 실제 검사한 수의 총 개수 - b의 길이
+print(len(a) - len(b))
