e932a64e54ddf37dfc2b2690eda0af5775282810
p1aying <auddn0585@naver.com>
2025-04-13 14:53:48 +0900

    Title: 두 배열의 곱, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250413/두 배열의 곱/multiple-of-two-arrays.py
+++ 250413/두 배열의 곱/multiple-of-two-arrays.py
@@ -1,12 +1,11 @@
 flag=0
-for i in range(2):
-    if flag==0:
-        matrix1=[list(map(int,input().split()))for _ in range(3)]
-        flag+=1
-    else:
-        matrix2=[list(map(int,input().split()))for _ in range(3)]
-print(matrix1)
-print(matrix2)
+
+matrix1=[list(map(int,input().split()))for _ in range(3)]
+matrix2=[list(map(int,input().split()))for _ in range(4)]
+matrix2.pop(0)
+#print(matrix1)
+#print(matrix2)
+
 for ii in range(3):
     for iii in range(3):
         keke=matrix1[ii][iii]*matrix2[ii][iii]
