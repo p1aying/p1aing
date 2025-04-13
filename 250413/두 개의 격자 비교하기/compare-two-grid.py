107cdbd48140638518e824fe65f809d04f43d668
p1aying <auddn0585@naver.com>
2025-04-13 15:14:21 +0900

    Title: 두 개의 격자 비교하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250413/두 개의 격자 비교하기/compare-two-grid.py
+++ 250413/두 개의 격자 비교하기/compare-two-grid.py
@@ -1,18 +1,23 @@
-row,curm=map(int,input().split())
+row, curm = map(int, input().split())
 
-matrix1=[list(map(int,input().split()))for _ in range(row)]
-matrix2=[list(map(int,input().split()))for _ in range(row)]
-#print(matrix1)
-#print(matrix2)
-new_matrix=[[0,0,0,0] for _ in range(row)]
+matrix1 = [list(map(int, input().split())) for _ in range(row)]
+matrix2 = [list(map(int, input().split())) for _ in range(row)]
+
+# 디버깅용: 입력 확인
+assert len(matrix1) == row
+assert all(len(r) == curm for r in matrix1)
+assert len(matrix2) == row
+assert all(len(r) == curm for r in matrix2)
+
+new_matrix = [[0 for _ in range(curm)] for _ in range(row)]
+
 for i in range(row):
-    for ii in range(curm):
-        if matrix1[i][ii]==matrix2[i][ii]:
-            new_matrix[i][ii]=0
-        else:
-            new_matrix[i][ii]=1
-
-for iii in range(row):
     for j in range(curm):
-        print(new_matrix[iii][j], end=" ")
-    print()
+        if matrix1[i][j] == matrix2[i][j]:
+            new_matrix[i][j] = 0
+        else:
+            new_matrix[i][j] = 1
+
+# 출력
+for row in new_matrix:
+    print(*row)
