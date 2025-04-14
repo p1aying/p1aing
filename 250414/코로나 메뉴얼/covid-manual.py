1a2c9f4aa84ae913011af64317e97b47fd50d979
p1aying <auddn0585@naver.com>
2025-04-14 22:52:14 +0900

    Title: 코로나 메뉴얼, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250414/코로나 메뉴얼/covid-manual.py
+++ 250414/코로나 메뉴얼/covid-manual.py
@@ -5,11 +5,11 @@ a[1],b[1]=map(str,input().split())
 a[2],b[2]=map(str,input().split())
 count=0
 for i in range(3):
-    if a[i]=="Y" and int(b[i])>=40:
-        print("E")
+    if (a[i]=="Y" and int(b[i])>=37):
         count+=1
-        break
     else:
         continue
-if count==0:
+if count>=2:
+    print("E")
+else:
     print("N")
