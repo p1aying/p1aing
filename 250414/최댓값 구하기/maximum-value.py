3ab7cac947b65819874e9a28c71ce3fa0ebef77d
p1aying <auddn0585@naver.com>
2025-04-14 22:44:03 +0900

    Title: 최댓값 구하기, Time: 39ms, Memory: 16MB, Status: Passed - Codetree

--- 250414/최댓값 구하기/maximum-value.py
+++ 250414/최댓값 구하기/maximum-value.py
@@ -1,8 +1,17 @@
 a,b,c=map(int,input().split())
 
-if a>=b and a>=c:
-    print(a)
-if b>=a and b>=c:
-    print(a)
-if c>=a and c>=b:
-    print(c)
+if a>=b:
+    if a>=c:
+        print(a)
+    else:
+        print(c)
+elif b>=a:
+    if b>=c:
+        print(b)
+    else:
+        print(c)
+elif c>=a:
+    if c>=b:
+        print(c)
+    else:
+        print(b)
