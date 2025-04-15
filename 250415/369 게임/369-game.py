82511cbcf4697b205f61644a6ece0279c161a27c
p1aying <auddn0585@naver.com>
2025-04-15 14:52:01 +0900

    Title: 369 게임, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250415/369 게임/369-game.py
+++ 250415/369 게임/369-game.py
@@ -7,7 +7,7 @@ for i in range(1,n+1):
         new_list=[]
     else:
         for ii in range(len(new_num)):
-            if new_num[ii]==("3" or "6" or "9"):
+            if new_num[ii]=="3"or new_num[ii]== "6" or new_num[ii]== "9":
                 print(0, end=" ")
                 count+=1
                 break
