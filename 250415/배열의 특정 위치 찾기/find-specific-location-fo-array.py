796833f5e1534e09ecdcd8a472d6fccd54ab0ba3
p1aying <auddn0585@naver.com>
2025-04-15 14:25:01 +0900

    Title: 배열의 특정 위치 찾기, Time: 40ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250415/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -0,0 +1,13 @@
+mylist=list(map(int,input().split()))
+
+mysum=0
+mysum2=0
+count=0
+for i in range(len(mylist)):
+    if (i+1)%2==0:
+        mysum+=mylist[i]
+    if (i+1)%3==0:
+        mysum2+=mylist[i]
+        count+=1
+print(mysum,end=" ")
+print(mysum2/count)
