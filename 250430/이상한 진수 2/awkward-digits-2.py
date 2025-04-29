2ac243cf1424803de67a9f196dd4c11befab6581
p1aying <auddn0585@naver.com>
2025-04-30 00:11:10 +0900

    Title: 이상한 진수 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250430/이상한 진수 2/awkward-digits-2.py
+++ 250430/이상한 진수 2/awkward-digits-2.py
@@ -1,10 +1,28 @@
 a = input()
-num = list(a)
-
+num=list(a)
+num_temp=list(a)
+sum_list=[]
+sum=0
 for i in range(len(num)):
-    if num[i] == "0":
-        num[i] = "1"
-        break
+    if num[i]=="0":
+        num[i]="1"
+        k=0
+        for i in range(len(num)-1,-1,-1):
+            sum+=(int(num[k])*(2**i))
+            #print(sum)
+            k+=1
+        sum_list.append(sum)
+        sum=0
+    elif num[i]=="1":
+        num[i]="0"
+        k=0
+        for i in range(len(num)-1,-1,-1):
+            sum+=(int(num[k])*(2**i))
+            #print(sum)
+            k+=1
+        sum_list.append(sum)
+        sum=0
+    num=num_temp
+    #print(num)
+print(max(sum_list))
 
-binary_str = ''.join(num)
-print(int(binary_str, 2))
